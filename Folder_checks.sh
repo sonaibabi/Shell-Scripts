a97af01ef7d960ddf63b31b27089398ca4cc9f10
sonaibabi <56513615+sonaibabi@users.noreply.github.com>
2022-02-26 21:06:56 +0530

    Update Folder_checks.sh

--- Folder_checks.sh
+++ Folder_checks.sh
@@ -1,3 +1,7 @@
+Sripts will Monitor app log folder and make sure always only 10 files will present in the folder and  it will delete all the extra file. The sortin will be based on
+time of modification
+==========================================================
+
 #!/bin/sh
 folder_path="/home/deekshithsn1/application_logs"
 cd $folder_path
